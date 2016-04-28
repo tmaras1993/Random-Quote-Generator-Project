2deb3af254c528ef7ace5de42dac18723127c0c1
tmaras1993 <tmaras1993@gmail.com>
2016-04-28 07:11:40 -0500

    Uploaded initial files

---
+++ Project/js/script.js
@@ -0,0 +1,52 @@
+/* script that generates a random quote when a button is clicked
+    a quote, the one who made the quote, the place it was said, and the year it was first said
+    everytime you a new quote is displayed the background color changes
+    */
+var randomQuote = [
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: '',
+    type: ''
+  },
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: '',
+    type: ''
+  },
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: '',
+    type: ''
+  },
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: '',
+    type: ''
+  },
+  {
+    quote: '',
+    source: '',
+    citation: '',
+    year: '',
+    type: ''
+  },
+]
+// object that contains all the parts of the random quote in an array
+
+function getRandomQuote () {
+  return Math.random()
+}
+//pulls a random quote from the array when called
+
+function printQuote () {
+  document.write()
+}
+// prints the random quote to the screen when the button is clicked
